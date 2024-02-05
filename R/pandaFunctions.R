772b8ce643e11295838b7cbdebab7720694e9ada
holdind <holdind@gmail.com>
2024-02-05 15:06:02 -0500

    first commit

---
+++ R/pandaFunctions.R
@@ -0,0 +1,36 @@
+#' Convert a numeric grade vector to a character grade vector
+#'
+#' This function takes a numeric grade vector or column and converts it to a
+#' character. For example, -2 is converted to 'K0', 0 is converted to 'K2', 9 to
+#' '09', and 11 to '11'
+#'
+#' @param vector a numeric grade vector (kindergarten expressed as 0, -1, -2)
+#' @return a character grade vector (K0,K1,K2,01,02)
+#' @export
+
+kinderNumToStr <- function(vector) {
+  ifelse(
+    vector < 1,
+    sprintf('K%s',vector+2),
+    stringr::str_pad(vector,2,pad = '0')
+  )
+}
+
+#' Convert a character grade vector to a numeric grade vector
+#'
+#' This function takes a character grade vector or column and converts it to a
+#' number For example, 'K0' is converted to -2, 'K2' is converted to 0, '09' to
+#' 9, and '11' to 11
+#'
+#' @param vector a character grade vector ('K0','K1','K2','01','02')
+#' @return a numeric grade vector (-2,-1,0,1,2)
+#' @export
+
+kinderStrToNum <- function(vector) {
+  ifelse(
+    grepl('K',vector),
+    as.numeric(stringr::str_sub(vector,2,2))-2,
+    as.numeric(vector)
+  )
+}
+
---
+++ R/titanFunctions.R
@@ -0,0 +1,130 @@
+# Titan Functions. These functions are intended to help address some of the
+# shortcomings of the Titan system, like only producing reports in html and
+# doing other psychotic practices like formatting reports real badly. I will
+# also use it to fix the terrible practice of building the word DISCONTINUED
+# into keys
+
+#' Import and clean the Warehouse transfers report
+#'
+#' This function imports the badly formatted Titan Warehouse Transfer Report and
+#' moves the product and product identifier to the same row as the transfer
+#' reports.
+#'
+#' @param inFile This is the directory of an HTML Warehouse Transfers Report
+#' @return A data frame with the product and identifier values appropriately placed
+#' @export
+
+cleanWHTransfers <- function(inFile) {
+  step1 <- readTitanHTML(whTransfersURL) %>%
+    mutate(
+      internalHeader = ifelse(
+        expectedDate == quantity,1,0
+      ),
+      idGrouping = cumsum(intrnalHeader)
+    )
+
+  idTable <- step1 %>%
+    filter(internalHeader == 1) %>%
+    select(idGrouping,receivingBuilding) %>%
+    rename(headerString = receivingBuilding) %>%
+    mutate(
+      identifier = str_extract(headerString,'\\(([^)]+)\\)'),
+      identifier = removeDiscontinued(identifier),
+      identifier = str_trim(gsub('[[:punct:] ]+','',identifier)),
+
+      product = sub('.*?\\)','',headerString),
+      product = sub('^\\)','',product),
+      product = str_trim(product)
+    ) %>% select(-headString)
+
+  step1 %>%
+    filter(internalHeader == 0) %>%
+    left_join(
+      idTable, by = 'idGrouping'
+    ) %>%
+    mutate(
+      expectedDate = as.Date(expectedDate, format = '%b %d, %Y')
+    )
+}
+
+#' Convert an HTML report to a CSV
+#'
+#' This function imports a Titan HTML report and converts it to a CSV
+#'
+#' @param infile Path to the input file
+#' @return a csv file in the same directory as the html file
+#' @export
+
+convertTitanHTML <-  function(inFile) {
+  outFile <- inFile %>%
+    rvest::read_html() %>%
+    rvest::html_table() %>%
+    .[[1]] %>%
+    janitor::clean_names(case='lower_camel')
+
+  outFileDir <- sprintf(
+    '%s.csv',
+    tools::file_path_sans_ext(inFile)
+  )
+
+  write.csv(
+    outFile,
+    outFileDir,
+    na = '',
+    row.names = F
+  )
+}
+
+
+#' Import a Titan HTML report
+#'
+#' This function simplifies the process of importing and cleaning a report from
+#' Titan
+#'
+#' @param infile Path to the saved Titan HTML report
+#' @return A data frame containing the content of the Titan Report
+#' @export
+
+readTitanHTML <- function(inFile) {
+  inFile %>%
+    rvest::read_html() %>%
+    rvest::html_table() %>%
+    .[[1]] %>%
+    janitor::clean_names(case='lower_camel')
+}
+
+
+#' Remove DISCONTINUED string from the Identifier column
+#'
+#' This function addresses the bad data practice of altering product keys in our
+#' inventory database by writing DISCONTINUED after them when they cease using
+#' them. It's pretty simple, and removes everything after the first space in a
+#' string.
+#'
+#' @param vector A vector to remove the "Discontinued" string from
+#' @return A vector with 'Discontinued' removed from all of the strings. More
+#'   specifically, a vector with everything after the first space removed.
+#' @export
+
+removeDiscontinued <- function(vector) {
+  returnVector <- sub('\\s(.+)','',vector)
+  return(returnVector)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
