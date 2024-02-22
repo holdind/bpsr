5b436b2c45c413107b8b1abe32d8492ecddc9b8c
Dane Holding <holdind@gmail.com>
2024-02-22 14:51:46 -0500

    function rename

--- r/titanFunctions.R
+++ r/titanFunctions.R
@@ -7,122 +7,6 @@
 #' @import tidyverse tools janitor rvest
 NULL
 
-#' Import and clean the Warehouse transfers report
-#'
-#' This function imports the badly formatted Titan Warehouse Transfer Report and
-#' moves the product and product identifier to the same row as the transfer
-#' reports.
-#'
-#' @param inFile This is the directory of an HTML Warehouse Transfers Report
-#' @return A data frame with the product and identifier values appropriately placed
-#' @export
-
-cleanWHTransfers <- function(inFile) {
-  step1 <- readTitanHTML(inFile) %>%
-    mutate(
-      internalHeader = ifelse(
-        expectedDate == quantity,1,0
-      ),
-      idGrouping = cumsum(internalHeader)
-    )
-
-  idTable <- step1 %>%
-    filter(internalHeader == 1) %>%
-    select(idGrouping,receivingBuilding) %>%
-    rename(headerString = receivingBuilding) %>%
-    mutate(
-      identifier = str_extract(headerString,'\\(([^)]+)\\)'),
-      identifier = removeDiscontinued(identifier),
-      identifier = str_trim(gsub('[[:punct:] ]+','',identifier)),
-
-      product = sub('.*?\\)','',headerString),
-      product = sub('^\\)','',product),
-      product = stringr::str_trim(product),
-
-    ) %>% select(-headerString)
-
-  step1 %>%
-    filter(internalHeader == 0) %>%
-    left_join(
-      idTable, by = 'idGrouping'
-    ) %>%
-    mutate(
-      expectedDate = as.POSIXct(expectedDate, format = '%b %d, %Y', tz = 'UTC'),
-      quantity = as.numeric(quantity)
-    ) %>%
-    select(
-      -internalHeader,-idGrouping
-    )
-}
-
-#' Convert an HTML report to a CSV
-#'
-#' This function imports a Titan HTML report and converts it to a CSV
-#'
-#' @param inFile Path to the input file
-#' @return a csv file in the same directory as the html file
-#' @export
-
-convertTitanHTML <-  function(inFile) {
-  outFile <- readTitanHTML(inFile)
-
-  outFileDir <- sprintf(
-    '%s.csv',
-    tools::file_path_sans_ext(inFile)
-  )
-
-  write.csv(
-    outFile,
-    outFileDir,
-    na = '',
-    row.names = F
-  )
-}
-
-#' Clean and convert a Warehouse Transfers report to csv
-#'
-#' This function imports an HTML Warehouse Transfers Report, converts it to a
-#' friendlier format, then exports it as a csv in the same directory.
-#'
-#' @param inFile Path to the input file
-#' @return a csv file in the same directory as the html file
-#' @export
-
-convertWHTransfers <- function(inFile) {
-  outFile <- cleanWHTransfers(inFile)
-
-  outFileDir <- sprintf(
-    '%s.csv',
-    tools::file_path_sans_ext(inFile)
-  )
-
-  write.csv(
-    outFile,
-    outFileDir,
-    na = '',
-    row.names = F
-  )
-}
-
-
-#' Import a Titan HTML report
-#'
-#' This function simplifies the process of importing and cleaning a report from
-#' Titan
-#'
-#' @param inFile Path to the saved Titan HTML report
-#' @return A data frame containing the content of the Titan Report
-#' @export
-
-readTitanHTML <- function(inFile) {
-  inFile %>%
-    rvest::read_html() %>%
-    rvest::html_table() %>%
-    .[[1]] %>%
-    janitor::clean_names(case='lower_camel')
-}
-
-
 #' Remove DISCONTINUED string from the Identifier column
 #'
 #' This function addresses the bad data practice of altering product keys in our
@@ -181,20 +65,143 @@ sepMultipleInventory <- function(df, var) {
 #' @param df A Titan report containing a string units column formatted 'n
 #'   units'.
 #' @param var The Variable containing a string units column formatted 'n units'.
+#' @param nameTotal What you want to name the new total items variable.
+#' @param nameUnits What you want to name the new units variable
 #' @return a data frame with a string units column and a numeric total units
 #'   column.
 #' @export
 
-sepQuantAndUnit <- function(df, var) {
+sepQuantAndUnit <- function(df, var, nameTotal, nameUnits) {
+
+  strTotal <- deparse(substitute(nameTotal))
+  strUnits <- deparse(substitute(nameUnits))
 
   df %>%
     separate(
-      {{ var }}, into = c('quantity','unitOfMeasure'),
+      {{ var }}, into = c(strTotal,strUnits),
       extra = 'merge',
       sep = ' '
     ) %>%
-    mutate(quantity = as.numeric(quantity))
+    mutate({{ nameTotal }} := as.numeric({{ nameTotal }}))
 
+}
+
+
+#' Convert an HTML report to a CSV
+#'
+#' This function imports a Titan HTML report and converts it to a CSV
+#'
+#' @param inFile Path to the input file
+#' @return a csv file in the same directory as the html file
+#' @export
+
+titanConvertHTML <-  function(inFile) {
+  outFile <- titanImportHTML(inFile)
+
+  outFileDir <- sprintf(
+    '%s.csv',
+    tools::file_path_sans_ext(inFile)
+  )
+
+  write.csv(
+    outFile,
+    outFileDir,
+    na = '',
+    row.names = F
+  )
+}
+
+
+#' Clean and convert a Warehouse Transfers report to csv
+#'
+#' This function imports an HTML Warehouse Transfers Report, converts it to a
+#' friendlier format, then exports it as a csv in the same directory.
+#'
+#' @param inFile Path to the input file
+#' @return a csv file in the same directory as the html file
+#' @export
+
+titanConvertWHTransfers <- function(inFile) {
+  outFile <- cleanWHTransfers(inFile)
+
+  outFileDir <- sprintf(
+    '%s.csv',
+    tools::file_path_sans_ext(inFile)
+  )
+
+  write.csv(
+    outFile,
+    outFileDir,
+    na = '',
+    row.names = F
+  )
+}
+
+
+#' Import a Titan HTML report
+#'
+#' This function simplifies the process of importing and cleaning a report from
+#' Titan
+#'
+#' @param inFile Path to the saved Titan HTML report
+#' @return A data frame containing the content of the Titan Report
+#' @export
+
+titanImportHTML <- function(inFile) {
+  inFile %>%
+    rvest::read_html() %>%
+    rvest::html_table() %>%
+    .[[1]] %>%
+    janitor::clean_names(case='lower_camel')
+}
+
+
+#' Import and clean the Warehouse transfers report
+#'
+#' This function imports the badly formatted Titan Warehouse Transfer Report and
+#' moves the product and product identifier to the same row as the transfer
+#' reports.
+#'
+#' @param inFile This is the directory of an HTML Warehouse Transfers Report
+#' @return A data frame with the product and identifier values appropriately placed
+#' @export
+
+titanImportWHTransfers <- function(inFile) {
+  step1 <- titanImportHTML(inFile) %>%
+    mutate(
+      internalHeader = ifelse(
+        expectedDate == quantity,1,0
+      ),
+      idGrouping = cumsum(internalHeader)
+    )
+
+  idTable <- step1 %>%
+    filter(internalHeader == 1) %>%
+    select(idGrouping,receivingBuilding) %>%
+    rename(headerString = receivingBuilding) %>%
+    mutate(
+      identifier = str_extract(headerString,'\\(([^)]+)\\)'),
+      identifier = removeDiscontinued(identifier),
+      identifier = str_trim(gsub('[[:punct:] ]+','',identifier)),
+
+      product = sub('.*?\\)','',headerString),
+      product = sub('^\\)','',product),
+      product = stringr::str_trim(product),
+
+    ) %>% select(-headerString)
+
+  step1 %>%
+    filter(internalHeader == 0) %>%
+    left_join(
+      idTable, by = 'idGrouping'
+    ) %>%
+    mutate(
+      expectedDate = as.POSIXct(expectedDate, format = '%b %d, %Y', tz = 'UTC'),
+      quantity = as.numeric(quantity)
+    ) %>%
+    select(
+      -internalHeader,-idGrouping
+    )
 }
 
 
