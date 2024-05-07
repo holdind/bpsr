8b792d796dc588c5dadddb16a754341e06e897bf
Dane Holding <holdind@gmail.com>
2024-05-07 16:54:21 -0400

    school schedule functions
    school schedule functions live!

--- R/schYearFunctions.R
+++ R/schYearFunctions.R
@@ -4,8 +4,124 @@
 # also use it to fix the terrible practice of building the word DISCONTINUED
 # into keys
 
-#' @import lubridate stringr
+#' @import lubridate stringr RCurl tidyverse
 NULL
+
+
+#' Import the days off calendar from Github
+#'
+#' Having quick access to a school year calendar is extremely valuable. To do
+#' that, We need to have access to the days off so that we can remove them from
+#' the calendar.
+#'
+#' @return the days off data set, which includes calendar days off and an
+#'   explanation for the event
+#' @export
+
+bpsSchedDaysOff <- function() {
+
+  githubDir <- RCurl::getURL('https://raw.githubusercontent.com/holdind/bpsr/main/data/bpsHolidays.csv')
+
+  daysOff <- read.csv(text = githubDir) %>%
+    dplyr::mutate(date = as.POSIXct(date,format = '%m/%d/%Y', tz = 'UTC'))
+
+  return(daysOff)
+
+}
+
+#' Import a data set containing all scheduled school days in Boston Public
+#' Schools for the upcoming school year
+#'
+#' This command creates a dataframe containing a list of all scheduled school
+#' dates. It provides an option for a selective date range.
+#'
+#' @return a dataframe containing dates for the selected date range, including
+#'   fiscYear, date, and day of the week.
+#' @param startYear a numeric value representing the first fiscal year to pull.
+#'   Note that a fiscal year is the second year in a school year, so the fiscal
+#'   year for 2022-23 would be 2023.
+#' @param endYear a numeric value representing the last fiscal year to pull.
+#'   Note that a fiscal year is the second year in a school year, so the fiscal
+#'   year for 2022-23 would be 2023.
+#' @export
+
+bpsScheduleDates <- function(startYear=NA,endYear=NA) {
+
+  githubDir <- RCurl::getURL('https://raw.githubusercontent.com/holdind/bpsr/main/data/bpsCalendarLengths.csv')
+  posixDayLength = 60*60*24
+
+  # drop days off
+  daysOff <- bpsSchedDaysOff()
+
+  # drop weekends
+  weekend <- c('Saturday','Sunday')
+
+  baseDF <- read.csv(text = githubDir)
+
+  if(!is.na(startYear)) {
+    if(!is.na(endYear)) {
+      baseDF <- baseDF %>%
+        dplyr::filter(fiscYear>=startYear & fiscYear<=endYear)
+    } else {
+      baseDF <- baseDF %>%
+        dplyr::filter(fiscYear>=startYear)
+    }
+  } else if(!is.na(endYear)) {
+    baseDF <- baseDF %>%
+      dplyr::filter(fiscYear<=endYear)
+  }
+
+  baseDFLen <- nrow(baseDF)
+  if(baseDFLen < 1) {
+    stop('selected school years unavailable')
+  }
+
+  df <- baseDF %>%
+    dplyr::mutate(
+      start = as.POSIXct(start,format = '%m/%d/%Y', tz = 'UTC'),
+      end = as.POSIXct(end,format = '%m/%d/%Y', tz = 'UTC'),
+      range = as.numeric(end-start+1)
+    ) %>%
+    tidyr::uncount(range) %>%
+    dplyr::mutate(
+      date = start+row_number()*posixDayLength-posixDayLength,
+      dow = weekdays(date)
+    ) %>%
+    dplyr::select(fiscYear,date,dow) %>%
+    dplyr::filter(!(dow %in% weekend)) %>%
+    dplyr::anti_join(daysOff, by = 'date')
+
+  return(df)
+
+}
+
+#' return a dataframe containing the number of school days in each month in BPS
+#'
+#' This command returns a data frame containing the fiscal year, month, and
+#' total school days each month in BPS
+#'
+#' @return a dataframe containing fiscal year, month, and the total school days
+#'   in the month
+#' @param startYear a numeric value representing the first fiscal year to pull.
+#'   Note that a fiscal year is the second year in a school year, so the fiscal
+#'   year for 2022-23 would be 2023.
+#' @param endYear a numeric value representing the last fiscal year to pull.
+#'   Note that a fiscal year is the second year in a school year, so the fiscal
+#'   year for 2022-23 would be 2023.
+#' @export
+
+bpsMonthDateCount <- function(startYear=NA,endYear=NA) {
+
+  scheduleData <- bpsScheduleDates(startYear=startYear,endYear=endYear)
+
+  df = scheduleData %>%
+    mutate(month = yyyymmString(date)) %>%
+    group_by(fiscYear,month) %>%
+    summarise(totalDays = n())
+
+  return(df)
+
+}
 
 #' Extract the current fiscal year from a date value
 #'
@@ -74,3 +190,55 @@ schYearFromDate <- function(date) {
   return(schYear)
 
 }
+
+#' Build a date string formated YYYYMMDD
+#'
+#' This function returns a date string formatted YYYYMMDD for use in file
+#' directories & file names.
+#'
+#' @param date the day you want to convert to the string
+#' @return a string with the current date formatted YYYYMMDD
+#' @export
+
+yyyymmddString <- function(date) {
+
+  s <- sprintf(
+    '%s%s',
+    stringr::str_remove(yyyymmString(date),'\\-'),
+    stringr::str_pad(lubridate::day(date),2,pad = '0')
+  )
+
+  return(s)
+
+}
+
+#' Build a current date string formated YYYY-MM
+#'
+#' This function returns a date string formatted YYYY-MM for use in file
+#' directories & file names.
+#'
+#' @param date the day you want to convert to the string
+#' @return a string with the date formatted YYYY-MM
+#' @export
+
+yyyymmString <- function(date) {
+
+  s <- sprintf(
+    '%s-%s',
+    lubridate::year(date),
+    stringr::str_pad(lubridate::month(date),2,pad = '0')
+  )
+
+  return(s)
+
+}
+
+
+
+
+
+
+
+
+
+
